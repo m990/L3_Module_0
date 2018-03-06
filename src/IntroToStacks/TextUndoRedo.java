a0517f20322c0ec47a5eff00f195672fdd726645
ryangleason <league@WorthyMini-6.attlocal.net>
2018-03-05 20:01:16 -0800

    IntroToStacks

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,14 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +21,54 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
+	Stack<String> key = new Stack<String>();
+	Stack<String> lastValues = new Stack<String>();
+	JLabel label = new JLabel();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	String last = "";
+	public static void main(String[] args) {
+		TextUndoRedo tur = new TextUndoRedo();
+		tur.panel.add(tur.label);
+		tur.frame.add(tur.panel);
+		tur.frame.setVisible(true);
+		tur.frame.setSize(100, 50);
+		tur.frame.addKeyListener(tur);
+		tur.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tur.label.setText(""+tur.key);
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		String temp = "";
+		if (e.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
+			if (!key.isEmpty()) {
+				last = key.pop();
+				lastValues.push(last);
+			}
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			if (key.isEmpty()) {
+				last = lastValues.pop();
+				key.push(last);
+				last = "";
+			}
+		}
+		else {
+			key.push(""+e.getKeyChar());
+			System.out.println(e.getKeyChar());
+		}
+		for (String s: key) {
+			temp += s;
+		}
+		label.setText(temp);
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
 }
